61908275c5e1d2c06fb37274d2aa98d39267a353
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-03-06 13:00:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorshipTogether::Application.config.secret_key_base = '8f379805ab787d44d1778103987e9a47c443c6a7833431dd115deb9adf980e461202c34b814ff967b89fdd07b7c488399c3f6c131f44e2887450d3f4821b6b1a'

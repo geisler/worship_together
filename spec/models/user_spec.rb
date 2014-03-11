e64c0dae6cc920a3dd3d2d5f83215b2fe1771f7d
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-03-11 13:52:14 -0400

    Created User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe User do
+    let(:user) { User.new }
+    subject { user }
+
+    it { should respond_to(:name) }
+    it { should respond_to(:email) }
+    it { should respond_to(:password) }
+end

fc3b5add4e9ca542af96691ebcdd7656e0eaf719
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-03-11 15:23:59 -0400

    Validate User attributes exist.
    Simple tests and code to ensure that all user attributes exist and
    have a non-blank value.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,48 @@
 require 'spec_helper'
 
 describe User do
-    let(:user) { User.new }
+    let(:user) { User.new(name: 'John Doe', email: 'jdoe@example.com', password: 'password') }
     subject { user }
 
     it { should respond_to(:name) }
     it { should respond_to(:email) }
     it { should respond_to(:password) }
+
+    it { should be_valid }
+
+    describe "empty name" do
+	before { user.name = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank name" do
+	before { user.name = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty email" do
+	before { user.email = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank email" do
+	before { user.email = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty password" do
+	before { user.password = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank password" do
+	before { user.password = ' ' }
+
+	it { should_not be_valid }
+    end
 end

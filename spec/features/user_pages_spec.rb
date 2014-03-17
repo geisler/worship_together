6a00bf82624caf79128db1e3ad816e2e36e1325b
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-03-17 15:01:41 -0400

    Add tests for creating new users
    We add several tests to check that new users can be created properly.
    Nothing special in that idea, but as part of the process, we added a
    helper function to the testing framework that checks for alert
    messages that are of the proper format named have_alert().  That
    method takes two parameters that include the alert type and any
    options that need to be passed on to have_selector().  The
    implementation is yet to be completed since the student does not
    currently know about flash messages and therefore what the content of
    the have_selector() call should even look like.  That will be
    completed in future commits.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -21,4 +21,45 @@ describe "User Pages" do
 	    end
 	end
     end
+
+    describe "creating user" do
+	before { visit new_user_path }
+
+	it "hides password text" do
+	    should have_field 'user_password', type: 'password'
+	end
+
+	describe "with invalid information" do
+	    it "does not add the user to the system" do
+		expect { click_button 'Submit' }.not_to change(User, :count)
+	    end
+
+	    it "produces an error message" do
+		click_button 'Submit'
+		should have_alert(:danger)
+	    end
+	end
+
+	describe "with valid information" do
+	    before do
+		fill_in 'Username', with: 'John Doe'
+		fill_in 'Email', with: 'john.doe@example.com'
+		fill_in 'Password', with: 'password'
+	    end
+
+	    it "allows the user to fill in the fields" do
+		click_button 'Submit'
+	    end
+
+	    it "does add the user to the system" do
+		expect { click_button 'Submit' }.to change(User, :count).by(1)
+	    end
+
+	    it "produces a welcome message" do
+		before { click_button 'Submit' }
+
+		it { should have_alert(:success, text: 'Welcome') }
+	    end
+	end
+    end
 end

f67a9e9460225afd6e8c740e3bc03ae8dad31951
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-03-19 14:03:33 -0400

    Allow a User to be created.
    We accept a user with name, email, and password to be created using
    the form from the new action.  All requests are automatically
    redirected to the index action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,9 @@ class UsersController < ApplicationController
     def new
 	@user = User.new
     end
+
+    def create
+	User.create(params.require(:user).permit(:name, :email, :password))
+	redirect_to users_path
+    end
 end

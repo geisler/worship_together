ae57a06afbcaad7318f882c0e090e97805cfd15d
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-04-07 12:50:58 -0400

    Refactor strong parameters into single method
    We call the strong parameters twice in the previous code and just DRY
    it to be used in a single place in the User controller.
    
    Conflicts:
    	app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     end
 
     def create
-	@user = User.new(params.require(:user).permit(:name, :email, :password))
+	@user = User.new(user_params)
 	if @user.save
 	    flash[:success] = "Welcome to the site, #{@user.name}"
 	    redirect_to @user
@@ -23,5 +23,11 @@ class UsersController < ApplicationController
     rescue
 	flash[:danger] = "Unable to find user"
 	redirect_to users_path
+    end
+
+    private
+
+    def user_params
+	params.require(:user).permit(:name, :email, :password)
     end
 end

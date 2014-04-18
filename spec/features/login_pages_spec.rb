6b6d54071f4575d38b4a769c296cf1207bf40b00
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-04-18 16:34:15 -0400

    Tests for login/logout functionality
    Simple tests to determine that our user can properly get in and out of
    the site the way we want them to be able to.

---
+++ spec/features/login_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "LoginPages" do
+    subject { page }
+
+    describe "SignIn page" do
+	before { visit new_login_path }
+
+	it { should have_content('Sign In') }
+
+	describe "with invalid account information" do
+	    before { click_button 'Log In' }
+
+	    it { should have_alert(:danger, text: 'Invalid') }
+	end
+
+	describe "with valid account information" do
+	    let (:user) { FactoryGirl.create(:user) }
+
+	    before do
+		fill_in 'Username', with: user.name
+		fill_in 'Password', with: user.password
+		click_button 'Log In'
+	    end
+
+	    it { should have_link('Log Out', href: login_path) }
+	    it { should_not have_link('Log In', href: new_login_path) }
+	    it { should have_alert(:success) }
+
+	    describe "followed by logout" do
+		before { click_link 'Log Out' }
+
+		it { should have_link('Log In', href: new_login_path) }
+		it { should_not have_link('Log Out', href: login_path) }
+		it { should have_alert(:info) }
+	    end
+	end
+    end
+end

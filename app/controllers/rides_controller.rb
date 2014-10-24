742aa6522ba28a470f879684d24855ddccf731f3
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-10-24 11:44:46 -0400

    Pass Ride ordering tests
    These simple tests make sure that the previously committed tests pass
    by adding the date to the Ride output and calling order on Ride with
    the proper value.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,12 @@
 class RidesController < ApplicationController
     def index
-	@rides = Ride.all
+	order_param = (params[:order] || :Date).to_sym
+	ordering = case order_param
+		   when :Date
+		       :date
+		   when :Service
+		       :service_id
+		   end
+	@rides = Ride.order(ordering)
     end
 end

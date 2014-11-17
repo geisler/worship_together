a9866ee8cc3e4c403c1516abd7bdb290ddebeb1e
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-11-17 11:21:06 -0500

    Only allow church managers to edit church profiles
    This tests that only church managers are allowed to be able to access
    the profiles of the various church profile pages that relate to
    edits/updates.  We don't support creating them, yet.  So this is an
    initial security step once they are created to ensure they're not
    modified by others.

--- spec/features/authorization_pages_spec.rb
+++ spec/features/authorization_pages_spec.rb
@@ -73,6 +73,26 @@ describe 'AuthorizationPages' do
 		end
 	    end
 	end
+
+	describe "for Churches controller" do
+	    describe "edit action" do
+		let (:church) { FactoryGirl.create(:church) }
+		let (:login_user) { FactoryGirl.create(:user) }
+		let (:error_type) { :danger }
+
+		it_behaves_like "redirects to root" do
+		    let (:browser_path) { edit_church_path(church) }
+		    let (:error_signature) { 'Edit church profile' }
+		    let (:direct_path) { church_path(church) }
+		    let (:direct_http_method) { :patch }
+		end
+
+		it_behaves_like "redirects to root", skip_browser: true do
+		    let (:direct_path) { church_path(church) }
+		    let (:direct_http_method) { :delete }
+		end
+	    end
+	end
     end
 
     describe "admin user" do

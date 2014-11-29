02ddea803d17e6843ef6c93c257d392685a90ab3
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-11-28 23:57:14 -0500

    Tests updated for nested ride inside service
    We need to nest the ride inside a service, so the existing tests that
    used ride routes need to be updated to reflect this situation.

--- spec/features/ride_pages_spec.rb
+++ spec/features/ride_pages_spec.rb
@@ -5,16 +5,15 @@ describe 'Ride Pages' do
 
     describe "show rides" do
 	describe "all" do
-	    before do
-		25.times { FactoryGirl.create(:ride) }
-		visit rides_path
-	    end
+	    let (:service) { FactoryGirl.create(:service, num_rides: 25) }
+
+	    before { visit service_rides_path(service) }
 
 	    it { should have_content('List of rides') }
-	    it { should have_content("#{Ride.count} rides") }
+	    it { should have_content("25 rides") }
 
 	    it "should show all rides" do
-		Ride.all.each do |ride|
+		service.rides.each do |ride|
 		    should have_selector('li', text: ride.user.name)
 		    should have_selector('li', text: ride.service.church.name)
 		    should have_selector('li', text: ride.date)
@@ -22,32 +21,37 @@ describe 'Ride Pages' do
 	    end
 
 	    describe "by date" do
-		let! (:middle) { FactoryGirl.create(:ride, date: '2014/10/19') }
-		let! (:last) { FactoryGirl.create(:ride, date: '2014/10/26') }
-		let! (:first) { FactoryGirl.create(:ride, date: '2014/10/12') }
+		let! (:middle) { FactoryGirl.create(:ride, date: '2014/10/19', service: service) }
+		let! (:last) { FactoryGirl.create(:ride, date: '2014/10/26', service: service) }
+		let! (:first) { FactoryGirl.create(:ride, date: '2014/10/12', service: service) }
 
-		before { visit rides_path(order: :Date) }
+		before { visit service_rides_path(service, order: :Date) }
 
 		it { should have_content(/#{first.date}.*#{middle.date}.*#{last.date}/) }
 	    end
 
-	    describe "by service" do
-		let! (:service1) { FactoryGirl.create(:service) }
-		let! (:service2) { FactoryGirl.create(:service) }
-		let! (:middle) { FactoryGirl.create(:ride,
-						    date: '2014/10/19',
-						    service: service2) }
-		let! (:last) { FactoryGirl.create(:ride,
-						  date: '2014/10/26',
-						  service: service1) }
-		let! (:first) { FactoryGirl.create(:ride,
-						   date: '2014/10/12',
-						   service: service1) }
-
-		before { visit rides_path(order: :Service) }
-
-		it { should have_content(/#{last.date}.*#{first.date}.*#{middle.date}/) }
-	    end
+#
+# Currently, this doesn't make sense since a ride is nested for a
+# particular service.  We could look into a different interface should
+# this type of output be necessary
+#
+#	    describe "by service" do
+#		let! (:service1) { FactoryGirl.create(:service) }
+#		let! (:service2) { FactoryGirl.create(:service) }
+#		let! (:middle) { FactoryGirl.create(:ride,
+#						    date: '2014/10/19',
+#						    service: service2) }
+#		let! (:last) { FactoryGirl.create(:ride,
+#						  date: '2014/10/26',
+#						  service: service1) }
+#		let! (:first) { FactoryGirl.create(:ride,
+#						   date: '2014/10/12',
+#						   service: service1) }
+#
+#		before { visit rides_path(order: :Service) }
+#
+#		it { should have_content(/#{last.date}.*#{first.date}.*#{middle.date}/) }
+#	    end
 	end
     end
 end

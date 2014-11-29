5c471ff4b93f7eca12ab5d9ac6351bfa7fcb2b65
Jonathan Geisler <jgeisler@cse.taylor.edu>
2014-11-29 00:07:35 -0500

    Tighter requirements on rides
    We now expect that the attributes on the ride are all required so that
    the ride can properly be specified by the user providing it.

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -28,4 +28,88 @@ describe Ride do
 
 	it { should_not be_valid }
     end
+
+    describe "empty date" do
+	before { ride.date = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank date" do
+	before { ride.date = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty leave time" do
+	before { ride.leave_time = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank leave time" do
+	before { ride.leave_time = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty return time" do
+	before { ride.return_time = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank return time" do
+	before { ride.return_time = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty number of seats" do
+	before { ride.number_of_seats = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank number of seats" do
+	before { ride.number_of_seats = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty seats available" do
+	before { ride.seats_available = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank seats available" do
+	before { ride.seats_available = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty meeting location" do
+	before { ride.meeting_location = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank meeting location" do
+	before { ride.meeting_location = ' ' }
+
+	it { should_not be_valid }
+    end
+
+    describe "empty vehicle" do
+	before { ride.vehicle = '' }
+
+	it { should_not be_valid }
+    end
+
+    describe "blank vehicle" do
+	before { ride.vehicle = ' ' }
+
+	it { should_not be_valid }
+    end
 end
